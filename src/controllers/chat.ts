5a6bea19a1e499bf35d92198a3b20fead84fb2ce
anandafiqri <anandafiqri1403@gmail.com>
2023-02-03 20:02:22 +0700

    refactoring

--- src/controllers/chat.ts
+++ src/controllers/chat.ts
@@ -1,31 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import Chat from "../models/chat";
-const chat = new Chat();
+const chat = new Chat().Chat;
 
 export default class Controller {
-  public static async postChat(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<void> {
-    try {
-      const { SenderId, message, image, RoomId } = req.body;
-
-      const data = await chat.createChat({
-        SenderId,
-        message,
-        image,
-        RoomId,
-      });
-
-      if (!data.acknowledge) throw { name: "Failed", msg: "failed post" };
-
-      res.status(200).json({ message: "success post" });
-    } catch (err) {
-      next(err);
-    }
-  }
-
   public static async renderChat(req: Request, res: Response): Promise<void> {
     const { user } = req.headers;
     res.render("chat", { user });
--- src/models/chat.ts
+++ src/models/chat.ts
@@ -1,6 +1,5 @@
 import { createSchema, Type, typedModel } from "ts-mongoose";
 import MongooseService from "../config/mongoose";
-import { ChatSchema } from "../interfaces/schema";
 import Encyption from "../helpers/crypto";
 import Room from "./room";
 const roomSchema = new Room().RoomSchema;
@@ -38,25 +37,4 @@ export default class Chat extends MongooseService {
   }
 
   public Chat = typedModel("Chat", this.ChatSchema);
-
-  protected getChat(data: ChatSchema) {
-    return new this.Chat(data);
-  }
-
-  public async createChat(data: ChatSchema): Promise<any> {
-    try {
-      return this.getChat(data).save();
-    } catch (err) {
-      console.log(err);
-      return err;
-    }
-  }
-
-  public async getData(): Promise<any> {
-    try {
-      return await this.Chat.aggregate([]);
-    } catch (err) {
-      return err;
-    }
-  }
 }
