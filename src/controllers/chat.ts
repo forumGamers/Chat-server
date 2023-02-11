97bee2d6563dc07afeac1473b75e00468ceda825
anandafiqri <anandafiqri1403@gmail.com>
2023-02-11 23:58:36 +0700

    get chat data

--- src/controllers/chat.ts
+++ src/controllers/chat.ts
@@ -4,7 +4,7 @@ import { Types } from "mongoose";
 import Encyption from "../helpers/crypto";
 
 export default class Controller {
-  public static async renderChat(
+  public static async getChat(
     req: Request,
     res: Response,
     next: NextFunction
@@ -12,14 +12,86 @@ export default class Controller {
     try {
       const { id } = req.headers;
       const { RoomId } = req.params;
+      let { sort, search, page, limit } = req.query;
+
+      let sorting: number;
+
+      switch (sort) {
+        case undefined:
+          sorting = 1;
+          break;
+
+        case "asc":
+          sorting = 1;
+          break;
+
+        case "desc":
+          sorting = -1;
+          break;
+        default:
+          sorting = 1;
+          break;
+      }
+
+      const query = {};
+      const searchQuery = [];
+
+      if (search) {
+        searchQuery.push({
+          $or: [
+            {
+              message: {
+                $regex: Encyption.encrypt(String(search)),
+                $options: "i",
+              },
+            },
+          ],
+        });
+      }
+
+      searchQuery.push({ RoomId: Types.ObjectId(RoomId) });
+
+      Object.assign(query, { $and: searchQuery });
+
+      const offset: number =
+        (page ? parseInt(page as string) : 1) *
+        (limit ? parseInt(limit as string) : 0);
 
       const data = await chatModel.aggregate([
         {
-          $match: {
-            _id: Types.ObjectId(RoomId),
+          $lookup: {
+            from: "rooms",
+            localField: "RoomId",
+            foreignField: "_id",
+            as: "room",
           },
         },
+        {
+          $unwind: { path: "$room", preserveNullAndEmptyArrays: true },
+        },
+        {
+          $match: query,
+        },
+        {
+          $sort: { createdAt: sorting },
+        },
+        {
+          $skip: offset,
+        },
+        {
+          $limit: limit ? parseInt(limit as string) : 10,
+        },
       ]);
+
+      if (!data) throw { name: "Data not found" };
+
+      const check: number = data[0]?.room?.users.findIndex(
+        (el: any) => el === Number(id)
+      );
+
+      if (check === -1) throw { name: "Forbidden" };
+
+      res.status(200).json(data);
     } catch (err) {
       next(err);
     }
@@ -43,9 +115,9 @@ export default class Controller {
 
       if (check === -1) throw { name: "Forbidden" };
 
-      if (!message && !image) {
+      if (!message && !image)
         throw { name: "bad request", msg: "please input message or image" };
-      } else if (!message) message = null;
+      else if (!message) message = null;
       else if (!image) image = null;
 
       await chatModel.create({
--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -4,7 +4,7 @@ import BaseRoutes from "./base";
 class ChatRouter extends BaseRoutes {
   routes(): void {
     this.router
-      .get("/:RoomId/", Controller.renderChat)
+      .get("/:RoomId/", Controller.getChat)
       .post("/:RoomId", Controller.postChat);
   }
 }
